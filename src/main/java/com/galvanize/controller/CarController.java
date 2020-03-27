ae148622d092e03bd7de8ad3f5bce81c5221738d
Asahi <asahimojica@yahoo.com>
2020-03-26 19:46:42 -0500

    test message

--- src/main/java/com/galvanize/controller/CarController.java
+++ src/main/java/com/galvanize/controller/CarController.java
@@ -1,7 +1,16 @@
 package com.galvanize.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/cars")
 public class CarController {
+    @GetMapping("/")
+    public String getCars(){
+        return "Hello Car.";
+    }
 }
+
+/// You go this message pa;lsdjfnaposjfpaosdjfpoasujpoausdjf
--- src/test/java/com/galvanize/controller/ControllerCarTest.java
+++ src/test/java/com/galvanize/controller/ControllerCarTest.java
@@ -1,14 +1,19 @@
 package com.galvanize.controller;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 @SpringBootTest
+@AutoConfigureMockMvc
 
 
 public class ControllerCarTest {
@@ -17,16 +22,17 @@ public class ControllerCarTest {
     @Autowired
     CarController carController;
     @Autowired
-    MockMvc mvc
+    MockMvc mvc;
 
     @Test
     void ControllerIsValid() {
         assertNotNull(carController);
     }
+
     @Test
+    void getDrivers() throws Exception {
 
-    void getDrivers()throws Exception{
-
+        mvc.perform(get("/api/cars"))
                 .andExpect(status().isOk());
     }
 }
